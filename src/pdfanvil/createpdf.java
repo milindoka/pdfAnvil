e573067a4d60c3f4d572ee3fd720bbb330af32cc
Milind Oka <oak444@gmail.com>
2018-02-22 20:33:57 +0530

    Started body of marklist which is pdfptable of 5 columns

--- src/pdfanvil/createpdf.java
+++ src/pdfanvil/createpdf.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font.FontFamily;
@@ -34,49 +35,152 @@ public class createpdf
 	    	String filename="hello.pdf";
 	    		
 	    	Document document = new Document(PageSize.A4);
+	    	   document.setMargins(2, 2, 2, 2);
 	    	PdfWriter.getInstance(document, new FileOutputStream(filename));
 	    	document.open();
 	    	
+	     //    com.itextpdf.text.Font NORMAL = new com.itextpdf.text.Font(FontFamily.TIMES_ROMAN, 12);
+	        AddHeader(document);
 	    	
-	    	
-	         com.itextpdf.text.Font NORMAL = new com.itextpdf.text.Font(FontFamily.TIMES_ROMAN, 12);
-	        	 
-	    	 
-	     //    Font TNR=new Font(TNR16)
-	     // Phrase phr=new Phrase("Test",TNR16);
-	      
-	    	PdfPTable table = new PdfPTable(3);
-	    	
-	    	PdfPCell cell = new PdfPCell(new Phrase("Test"));
-	    	cell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
-	    	table.addCell(cell);
-	    	
-	    	
-	    	cell = new PdfPCell(new Phrase("SIWS College"));
-	    	cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
-	    	table.addCell(cell);
-	    
-
-	    	cell = new PdfPCell(new Phrase("Test"));
-	    	cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
-	    	table.addCell(cell);
-	    	
-
-	    	 Float padding = 5f;    
-
-	    	 cell.setPaddingBottom(padding);
-	    	 
-	    /*	 
-	    	 for(int i=0;i<3;i++)
-	    	{
-	    	table.addCell(cell);
-	    	}
-	    */    	
-	    	document.add(table);
-	    
-	    	document.close();
-	    	
+            AddBody(document);
+            document.close();
 	    }
 	   	
 
+	  void AddHeader(Document document) throws DocumentException, IOException
+	  {PdfPTable table = new PdfPTable(3);
+	   
+  	PdfPCell cell = new PdfPCell(new Phrase(" "));
+  	cell.setBorder(PdfPCell.NO_BORDER);
+  	table.addCell(cell);
+  	
+  	
+  	cell = new PdfPCell(new Phrase("SIWS College"));cell.setBorder(PdfPCell.NO_BORDER);
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+  	table.addCell(cell);
+  
+
+  	cell = new PdfPCell(new Phrase(" "));cell.setBorder(PdfPCell.NO_BORDER);
+  	table.addCell(cell);
+
+  	
+  	cell = new PdfPCell(new Phrase("Class-Div :"));cell.setBorder(PdfPCell.NO_BORDER);
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
+  	table.addCell(cell);
+  	
+  	
+  	cell = new PdfPCell(new Phrase("Subject :"));cell.setBorder(PdfPCell.NO_BORDER);
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);cell.setBorder(PdfPCell.NO_BORDER);
+  	table.addCell(cell);
+  
+
+  	cell = new PdfPCell(new Phrase("Examiner :"));cell.setBorder(PdfPCell.NO_BORDER);
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+  	table.addCell(cell);
+
+
+
+  	cell = new PdfPCell(new Phrase("Examination :"));cell.setBorder(PdfPCell.NO_BORDER);
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
+  	table.addCell(cell);
+  	
+  	
+  	cell = new PdfPCell(new Phrase("Total :"));
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);cell.setBorder(PdfPCell.NO_BORDER);
+  	table.addCell(cell);
+  
+
+  	cell = new PdfPCell(new Phrase("Date :"));cell.setBorder(PdfPCell.NO_BORDER);
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+  	table.addCell(cell);
+    
+  	table.setSpacingAfter(10f);
+//  	 Float padding = 5f;    
+
+  //	 cell.setPaddingBottom(padding);
+  	 
+  /*	 
+  	 for(int i=0;i<3;i++)
+  	{
+  	table.addCell(cell);
+  	}
+  */    	
+     	document.add(table);
+		  
+		  
+	  }
+	  
+
+	  
+	  void AddBody(Document document) throws DocumentException, IOException
+	  {PdfPTable table = new PdfPTable(5);
+  	
+  	PdfPCell cell = new PdfPCell(new Phrase("Test"));
+  	
+  	/*
+  	table.addCell(cell);
+  	
+  	
+  	cell = new PdfPCell(new Phrase("SIWS College"));
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+  	table.addCell(cell);
+  
+
+  	cell = new PdfPCell(new Phrase(" "));
+  	table.addCell(cell);
+
+  	
+  	cell = new PdfPCell(new Phrase("Class-Div :"));
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
+  	table.addCell(cell);
+  	
+  	
+  	cell = new PdfPCell(new Phrase("Subject :"));
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+  	table.addCell(cell);
+  
+
+  	cell = new PdfPCell(new Phrase("Examiner :"));
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+  	table.addCell(cell);
+
+
+
+  	cell = new PdfPCell(new Phrase("Examination :"));
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
+  	table.addCell(cell);
+  	
+  	
+  	cell = new PdfPCell(new Phrase("Total :"));
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+  	table.addCell(cell);
+  
+
+  	cell = new PdfPCell(new Phrase("Date :"));
+  	cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+  	table.addCell(cell);
+    
+
+//  	 Float padding = 5f;    
+
+  //	 cell.setPaddingBottom(padding);
+  	 
+ */ 
+  	 for(int i=0;i<200;i++)
+  	{
+  	table.addCell(cell);
+  	}
+     	
+     	document.add(table);
+		  
+		  
+	  }
+	  
+
+	  
+	  
+	  
+	  
+	  
+	 
 }
