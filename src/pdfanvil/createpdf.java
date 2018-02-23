96d62fcc97c5a315ee8ece99701d0affb2783092
milindoka <oak445@gmail.com>
2018-02-23 11:42:31 +0530

    Tested Random marks in first inner table ( leftmost table in marklist)

--- src/pdfanvil/createpdf.java
+++ src/pdfanvil/createpdf.java
@@ -3,6 +3,8 @@ package pdfanvil;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -15,8 +17,12 @@ import com.itextpdf.text.pdf.PdfWriter;
 public class createpdf
 {
 	
-	
+	ArrayList<String> roll=new ArrayList<String>();//creating new generic arraylist  
+	ArrayList<String> mark=new ArrayList<String>();//creating new generic arraylist
 	//fylename=path+"/"+ReportName+"-Science.txt";
+	int strength=25;
+	
+	
 	public String getJarPath()
     {
     	File f = new File(System.getProperty("java.class.path"));
@@ -24,6 +30,29 @@ public class createpdf
         String path=dir.toString();
      	return  path;
     }
+	
+	
+	void FillMarksArray()
+	{   roll.removeAll(roll);
+	    mark.removeAll(mark);
+        int x=0;
+        String str;
+		Random ran = new Random();
+		for(int i=0;i<strength;i++)
+		  {
+			x = ran.nextInt(101);
+			str=String.format("%d", 5001+i);
+			roll.add(str); 
+			str=String.format("%d", x);
+			mark.add(str);
+			
+		  }
+
+	
+	
+	}
+	
+	
 	
 	
 	
@@ -37,9 +66,12 @@ public class createpdf
 	    	PdfWriter.getInstance(document, new FileOutputStream(filename));
 	    	document.open();
 	    	
+	    	
+	    	
 	     //    com.itextpdf.text.Font NORMAL = new com.itextpdf.text.Font(FontFamily.TIMES_ROMAN, 12);
 	        AddHeader(document);
-	    	
+	    	FillMarksArray();
+	        
             AddBody(document);
             document.close();
 	    }
@@ -108,14 +140,23 @@ public class createpdf
 	  
 	  void AddBody(Document document) throws DocumentException, IOException
 	  {PdfPTable table = new PdfPTable(5);
+	  float colwidth[]={6,3};
+  	  
+      PdfPTable table1 = new PdfPTable(colwidth);
+  	  table1.setWidthPercentage(96);
   	
-  	PdfPCell cell = new PdfPCell(new Phrase("Test"));
-  	float colwidth[]={6,3};
-  	 PdfPTable table1 = new PdfPTable(colwidth);
-  	table1.setWidthPercentage(96);
-  	for(int i=0;i<20;i++)
-  	{
-  	table1.addCell(cell);
+  	  
+  	  PdfPCell cell = new PdfPCell(new Phrase("Roll"));
+  	  table1.addCell(cell);
+  	  cell = new PdfPCell(new Phrase("Mrk"));
+  	  table1.addCell(cell);
+  	
+  	for(int i=0;i<strength;i++)
+  	{ cell = new PdfPCell(new Phrase(roll.get(i)));
+	  table1.addCell(cell);
+	  cell = new PdfPCell(new Phrase(mark.get(i)));
+	  table1.addCell(cell);
+  		
   	}
   	 
   	 PdfPCell firstTableCell = new PdfPCell();
